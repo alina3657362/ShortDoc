0f13def49ffc29495b1250d2de6bf691809b2cca
ElizavetaShikhova <elizaveta.liza06@mail.ru>
2026-06-13 13:43:19 +0500

    added cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
-
 from app.api.auth import router as auth_router
 from app.api.documents import router as documents_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ShortDoc API", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://shortdoc.website.yandexcloud.net",
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://81.26.184.194",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(documents_router)
